49999a5cef5409c9a7e8ad4511a8447e0e7d0252
arcnexus <marcmirallesbiosca@gmail.com>
2012-11-25 04:03:49 +0100

    Más tipos de búsqueda vademécum

--- frmanadirmedicamento.cpp
+++ frmanadirmedicamento.cpp
@@ -134,13 +134,15 @@ void FrmAnadirMedicamento::finishedSlotMedicamento(QNetworkReply* reply)
      ui->textEdit->setText((cXML));
 
 }
+// FIN BUSCAR MEDICAMENTO POR SU NOMBRE
+
 // BUSCAR MEDICAMENTO POR PATOLOGÍA
 
 void FrmAnadirMedicamento::on_btnBuscarMedicamentoporPatologia_clicked()
 {
    // ui->tablaPatologias->setCurrentCell(1,ui->tablaPatologias->currentRow(),NULL);
     QString cUrl = "http://demo.vademecumdata.es/vweb/xml/ws_indication/DrugsList?id_indication="+
-    ui->tablaPatologias->currentItem()->text();
+    ui->tablaPatologias->item(ui->tablaPatologias->currentRow(), 1)->text();
 
 
     // Recupero valores conexión Vademecum
@@ -178,11 +180,12 @@ void FrmAnadirMedicamento::finishedSlotBuscarMedicamentoPorPatologia(QNetworkRep
      QDomNode n = drugList.firstChild();
 
 
-     ui->tablamedicamentosPatologia->setColumnCount(3);
+     ui->tablamedicamentosPatologia->setColumnCount(4);
      ui->tablamedicamentosPatologia->setColumnWidth(0,400);
      ui->tablamedicamentosPatologia->setColumnWidth(1,0);
      ui->tablamedicamentosPatologia->setColumnWidth(2,450);
      ui->tablamedicamentosPatologia->setColumnWidth(3,200);
+     ui->tablamedicamentosPatologia->setColumnWidth(3,0);
 
     int nrow = 0;
     int pos = 0;
@@ -221,6 +224,13 @@ void FrmAnadirMedicamento::finishedSlotBuscarMedicamentoPorPatologia(QNetworkRep
                      newItem->setTextColor(Qt::green); // color de los items
                      ui->tablamedicamentosPatologia->setItem(pos,2,newItem);
                  }
+                 if (e.tagName() == "national_code") {
+                     QTableWidgetItem *newItem = new QTableWidgetItem(e.text());
+                     // para que los elementos no sean editables
+                     newItem->setFlags(newItem->flags() & (~Qt::ItemIsEditable));
+                     newItem->setTextColor(Qt::green); // color de los items
+                     ui->tablamedicamentosPatologia->setItem(pos,3,newItem);
+                 }
              }
 
              pos++;
@@ -231,10 +241,11 @@ void FrmAnadirMedicamento::finishedSlotBuscarMedicamentoPorPatologia(QNetworkRep
      }
 
 
-     ui->textEditPatologia->setText((cXML));
+    // ui->textProspecto->setText((cXML));
 
 }
-// Buscar Patología.
+// FIN BUSCAR MEDICAMENTOS POR PATOLOGÍA
+// BUSCAR PATOLOGIAS
 
 void FrmAnadirMedicamento::on_btnBuscarPatologia_clicked()
 {
@@ -249,7 +260,7 @@ void FrmAnadirMedicamento::on_btnBuscarPatologia_clicked()
 
     cUrl = cUrl + "&id_ent=" + cClave1;
 
-
+    qDebug() << cUrl;
         QNetworkAccessManager *manager = new QNetworkAccessManager(this);
         connect(manager, SIGNAL(finished(QNetworkReply*)),
                 this, SLOT(finishedSlotBuscarPatologia(QNetworkReply*)));
@@ -278,8 +289,8 @@ void FrmAnadirMedicamento::finishedSlotBuscarPatologia(QNetworkReply* reply)
 
 
      ui->tablaPatologias->setColumnCount(2);
-     ui->tablaPatologias->setColumnWidth(0,200);
-     ui->tablaPatologias->setColumnWidth(1,50);
+     ui->tablaPatologias->setColumnWidth(0,ui->tablaPatologias->width()-20);
+     ui->tablaPatologias->setColumnWidth(1,0);
 
 
     int nrow = 0;
@@ -322,6 +333,398 @@ void FrmAnadirMedicamento::finishedSlotBuscarPatologia(QNetworkReply* reply)
      }
 
 
-     ui->textEditPatologia->setText((cXML));
+     //ui->textProspecto->setText((cXML));
 
 }
+// FIN BUSCAR PATOLOGÍAS
+
+void FrmAnadirMedicamento::on_btnVerProspecto_clicked()
+{
+    ui->tabBuscarporPatologia->setCurrentIndex(1);
+
+}
+
+
+// BUSCAR EL PROSPECTO
+void FrmAnadirMedicamento::on_tablamedicamentosPatologia_clicked()
+{
+    QString cUrl = "http://demo.vademecumdata.es/vweb/xml/ws_prospecto/get_prospecto?national_code="+
+    ui->tablamedicamentosPatologia->item(ui->tablamedicamentosPatologia->currentRow(),3)->text();
+
+
+    // Recupero valores conexión Vademecum
+    QSettings settings("infint", "terra");
+    QString cClave1 = settings.value("Clave1").toString();
+    QString cClave2 = settings.value("Clave2").toString();
+
+    cUrl = cUrl + "&id_ent=" + cClave1;
+
+
+        QNetworkAccessManager *manager = new QNetworkAccessManager(this);
+        connect(manager, SIGNAL(finished(QNetworkReply*)),
+                this, SLOT(finishedSlotBuscarProspecto(QNetworkReply*)));
+
+       manager->get(QNetworkRequest(QUrl(cUrl)));
+}
+
+
+void FrmAnadirMedicamento::finishedSlotBuscarProspecto(QNetworkReply* reply)
+{
+    //qDebug()<<reply->readAll();
+     QString data=(QString)reply->readAll();
+     QString cXML = data;
+
+     // Extract values from XML
+     QDomDocument document("XmlDoc");
+     document.setContent(cXML);
+
+     QDomElement root = document.documentElement();
+     if (root .tagName() != "object")
+         qDebug("Bad root element.");
+
+     QDomNode drugList = root.firstChild();
+
+     QDomNode n = drugList.firstChild();
+     QDomNode n2 = n.firstChild();
+
+
+
+    int nrow = 0;
+    int pos = 0;
+     while (!n.isNull()) {
+         if (n.isElement()) {
+             QDomNodeList attributes = n.childNodes();
+
+             for (int i = 0; i < attributes.count(); i ++) {
+                 QDomElement e = attributes.at(i).toElement();
+
+                 if (e.tagName() == "prospecto") {
+
+                     while (!n2.isNull()) {
+                         if (n2.isElement()) {
+                             QDomNodeList attributes = n2.childNodes();
+
+                             for (int i = 0; i < attributes.count(); i ++) {
+                                 QDomElement e2 = attributes.at(i).toElement();
+
+                                 if (e2.tagName() == "body") {
+                                     ui->textEdit_2->setText(e.text());
+                                     ui->textProspecto->setText(e.text());
+                                 }
+                             }
+
+                             pos++;
+
+                             //data.append(s);
+                         }
+                         n2 = n2.nextSibling();
+                     }
+
+                 }
+             }
+
+             pos++;
+
+             //data.append(s);
+         }
+         n = n.nextSibling();
+     }
+
+
+     //ui->textProspecto->setText((cXML));
+
+}
+// FIN BUSCAR EL PROSPECTO
+
+void FrmAnadirMedicamento::on_tablaPatologias_doubleClicked()
+{
+    ui->btnBuscarMedicamentoporPatologia->click();
+}
+// BUSCAR UNA MOLÉCULA
+void FrmAnadirMedicamento::on_btnBuscarMolcula_clicked()
+{
+    QString cUrl = "http://demo.vademecumdata.es/vweb/xml/ws_substance/SearchByName?value="+
+    ui->txtBuscarMolecula->text().trimmed();
+
+
+    // Recupero valores conexión Vademecum
+    QSettings settings("infint", "terra");
+    QString cClave1 = settings.value("Clave1").toString();
+    QString cClave2 = settings.value("Clave2").toString();
+
+    cUrl = cUrl + "&id_ent=" + cClave1;
+
+    qDebug() << cUrl;
+        QNetworkAccessManager *manager = new QNetworkAccessManager(this);
+        connect(manager, SIGNAL(finished(QNetworkReply*)),
+                this, SLOT(finishedSlotBuscarMolecula(QNetworkReply*)));
+
+       manager->get(QNetworkRequest(QUrl(cUrl)));
+}
+
+void FrmAnadirMedicamento::finishedSlotBuscarMolecula(QNetworkReply* reply)
+{
+    //qDebug()<<reply->readAll();
+     QString data=(QString)reply->readAll();
+     QString cXML = data;
+
+     // Extract values from XML
+     QDomDocument document("XmlDoc");
+     document.setContent(cXML);
+
+     QDomElement root = document.documentElement();
+     if (root .tagName() != "object")
+         qDebug("Bad root element.");
+
+     QDomNode drugList = root.firstChild();
+
+     QDomNode n = drugList.firstChild();
+
+
+     ui->tablaMoleculas->setColumnCount(2);
+     ui->tablaMoleculas->setColumnWidth(0,ui->tablaMoleculas->width()-20);
+     ui->tablaMoleculas->setColumnWidth(1,0);
+
+
+    int nrow = 0;
+    int pos = 0;
+     while (!n.isNull()) {
+         if (n.isElement()) {
+             QDomNodeList attributes = n.childNodes();
+
+             for (int i = 0; i < attributes.count(); i ++) {
+                 QDomElement e = attributes.at(i).toElement();
+
+                 if (e.tagName() == "id_molecule") {
+                     //medicamentos.append(e.text());
+                     ui->tablaMoleculas->setRowCount(pos+1);
+                     QTableWidgetItem *newItem = new QTableWidgetItem(e.text());
+                     // para que los elementos no sean editables
+                     newItem->setFlags(newItem->flags() & (~Qt::ItemIsEditable));
+                     newItem->setTextColor(Qt::blue); // color de los items
+
+                     ui->tablaMoleculas->setItem(pos,1,newItem);
+
+
+                 }
+                 if (e.tagName() == "name_molecule") {
+                     //id.append(e.text());
+                     QTableWidgetItem *newItem = new QTableWidgetItem(e.text());
+                     // para que los elementos no sean editables
+                     newItem->setFlags(newItem->flags() & (~Qt::ItemIsEditable));
+                     newItem->setTextColor(Qt::blue); // color de los items
+                     ui->tablaMoleculas->setItem(pos,0,newItem);
+                 }
+
+             }
+
+             pos++;
+
+             //data.append(s);
+         }
+         n = n.nextSibling();
+     }
+
+
+     //ui->textProspecto->setText((cXML));
+
+}
+// FIN BUSCAR MOLÉCULAS
+
+// BUSCAR MEDICAMENTOS POR MOLÉCULA
+void FrmAnadirMedicamento::on_btnBuscarMedicamentoporMolecula_clicked()
+{
+    // ui->tablaPatologias->setCurrentCell(1,ui->tablaPatologias->currentRow(),NULL);
+     QString cUrl = "http://demo.vademecumdata.es/vweb/xml/ws_substance/DrugsList?id_molecule="+
+     ui->tablaMoleculas->item(ui->tablaMoleculas->currentRow(), 1)->text();
+
+
+     // Recupero valores conexión Vademecum
+     QSettings settings("infint", "terra");
+     QString cClave1 = settings.value("Clave1").toString();
+     QString cClave2 = settings.value("Clave2").toString();
+
+     cUrl = cUrl + "&id_ent=" + cClave1;
+
+
+         QNetworkAccessManager *manager = new QNetworkAccessManager(this);
+         connect(manager, SIGNAL(finished(QNetworkReply*)),
+                 this, SLOT(finishedSlotBuscarMedicamentoPorMolecula(QNetworkReply*)));
+
+        manager->get(QNetworkRequest(QUrl(cUrl)));
+
+ }
+
+ void FrmAnadirMedicamento::finishedSlotBuscarMedicamentoPorMolecula(QNetworkReply* reply)
+ {
+     //qDebug()<<reply->readAll();
+      QString data=(QString)reply->readAll();
+      QString cXML = data;
+
+      // Extract values from XML
+      QDomDocument document("XmlDoc");
+      document.setContent(cXML);
+
+      QDomElement root = document.documentElement();
+      if (root .tagName() != "object")
+          qDebug("Bad root element.");
+
+      QDomNode drugList = root.firstChild();
+
+      QDomNode n = drugList.firstChild();
+
+
+      ui->tablamedicamentosMolecula->setColumnCount(4);
+      ui->tablamedicamentosMolecula->setColumnWidth(0,400);
+      ui->tablamedicamentosMolecula->setColumnWidth(1,0);
+      ui->tablamedicamentosMolecula->setColumnWidth(2,450);
+      ui->tablamedicamentosMolecula->setColumnWidth(3,200);
+      ui->tablamedicamentosMolecula->setColumnWidth(3,0);
+
+     int nrow = 0;
+     int pos = 0;
+      while (!n.isNull()) {
+          if (n.isElement()) {
+              QDomNodeList attributes = n.childNodes();
+
+              for (int i = 0; i < attributes.count(); i ++) {
+                  QDomElement e = attributes.at(i).toElement();
+
+                  if (e.tagName() == "name_speciality") {
+                      //medicamentos.append(e.text());
+                      ui->tablamedicamentosMolecula->setRowCount(pos+1);
+                      QTableWidgetItem *newItem = new QTableWidgetItem(e.text());
+                      // para que los elementos no sean editables
+                      newItem->setFlags(newItem->flags() & (~Qt::ItemIsEditable));
+                      newItem->setTextColor(Qt::blue); // color de los items
+
+                      ui->tablamedicamentosMolecula->setItem(pos,0,newItem);
+
+
+                  }
+                  if (e.tagName() == "id_speciality") {
+                      //id.append(e.text());
+                      QTableWidgetItem *newItem = new QTableWidgetItem(e.text());
+                      // para que los elementos no sean editables
+                      newItem->setFlags(newItem->flags() & (~Qt::ItemIsEditable));
+                      newItem->setTextColor(Qt::red); // color de los items
+                      ui->tablamedicamentosMolecula->setItem(pos,1,newItem);
+                  }
+
+                  if (e.tagName() == "package") {
+                      QTableWidgetItem *newItem = new QTableWidgetItem(e.text());
+                      // para que los elementos no sean editables
+                      newItem->setFlags(newItem->flags() & (~Qt::ItemIsEditable));
+                      newItem->setTextColor(Qt::green); // color de los items
+                      ui->tablamedicamentosMolecula->setItem(pos,2,newItem);
+                  }
+                  if (e.tagName() == "national_code") {
+                      QTableWidgetItem *newItem = new QTableWidgetItem(e.text());
+                      // para que los elementos no sean editables
+                      newItem->setFlags(newItem->flags() & (~Qt::ItemIsEditable));
+                      newItem->setTextColor(Qt::green); // color de los items
+                      ui->tablamedicamentosMolecula->setItem(pos,3,newItem);
+                  }
+              }
+
+              pos++;
+
+              //data.append(s);
+          }
+          n = n.nextSibling();
+      }
+
+
+     // ui->textProspecto->setText((cXML));
+
+ }
+ // FIN BUSCAR MEDICAMENTOS POR MOLÉCULA
+
+void FrmAnadirMedicamento::on_tablamedicamentosMolecula_clicked()
+{
+    QString cUrl = "http://demo.vademecumdata.es/vweb/xml/ws_prospecto/get_prospecto?national_code="+
+    ui->tablamedicamentosMolecula->item(ui->tablamedicamentosMolecula->currentRow(),3)->text();
+    ui->textProspectoMolecula->setText("");
+
+    // Recupero valores conexión Vademecum
+    QSettings settings("infint", "terra");
+    QString cClave1 = settings.value("Clave1").toString();
+    QString cClave2 = settings.value("Clave2").toString();
+
+    cUrl = cUrl + "&id_ent=" + cClave1;
+
+
+        QNetworkAccessManager *manager = new QNetworkAccessManager(this);
+        connect(manager, SIGNAL(finished(QNetworkReply*)),
+                this, SLOT(finishedSlotBuscarProspectoMolecula(QNetworkReply*)));
+
+       manager->get(QNetworkRequest(QUrl(cUrl)));
+}
+
+
+void FrmAnadirMedicamento::finishedSlotBuscarProspectoMolecula(QNetworkReply* reply)
+{
+    //qDebug()<<reply->readAll();
+     QString data=(QString)reply->readAll();
+     QString cXML = data;
+
+     // Extract values from XML
+     QDomDocument document("XmlDoc");
+     document.setContent(cXML);
+
+     QDomElement root = document.documentElement();
+     if (root .tagName() != "object")
+         qDebug("Bad root element.");
+
+     QDomNode drugList = root.firstChild();
+
+     QDomNode n = drugList.firstChild();
+     QDomNode n2 = n.firstChild();
+
+
+
+    int nrow = 0;
+    int pos = 0;
+     while (!n.isNull()) {
+         if (n.isElement()) {
+             QDomNodeList attributes = n.childNodes();
+
+             for (int i = 0; i < attributes.count(); i ++) {
+                 QDomElement e = attributes.at(i).toElement();
+
+                 if (e.tagName() == "prospecto") {
+
+                     while (!n2.isNull()) {
+                         if (n2.isElement()) {
+                             QDomNodeList attributes = n2.childNodes();
+
+                             for (int i = 0; i < attributes.count(); i ++) {
+                                 QDomElement e2 = attributes.at(i).toElement();
+
+                                 if (e2.tagName() == "body") {
+                                     ui->textProspectoMolecula->setText(e.text());
+                                 }
+                             }
+
+                             pos++;
+
+                             //data.append(s);
+                         }
+                         n2 = n2.nextSibling();
+                     }
+
+                 }
+             }
+
+             pos++;
+
+             //data.append(s);
+         }
+         n = n.nextSibling();
+     }
+
+
+     //ui->textProspecto->setText((cXML));
+
+}
+// FIN BUSCAR EL PROSPECTO
--- frmanadirmedicamento.h
+++ frmanadirmedicamento.h
@@ -17,16 +17,34 @@ public:
     explicit FrmAnadirMedicamento(QWidget *parent = 0);
     ~FrmAnadirMedicamento();
     
+
+
+
+
 private slots:
     void on_pushButton_clicked();
     void finishedSlotMedicamento(QNetworkReply* reply);
     void finishedSlotBuscarMedicamentoPorPatologia(QNetworkReply* reply);
     void finishedSlotBuscarPatologia(QNetworkReply* reply);
-
-
+    void finishedSlotBuscarProspecto(QNetworkReply *reply);
+    void finishedSlotBuscarMolecula(QNetworkReply *reply);
+    void finishedSlotBuscarMedicamentoPorMolecula(QNetworkReply *reply);
+    void finishedSlotBuscarProspectoMolecula(QNetworkReply *reply);
     void on_btnBuscarMedicamentoporPatologia_clicked();
 
     void on_btnBuscarPatologia_clicked();
+
+    void on_btnVerProspecto_clicked();
+
+    void on_tablamedicamentosPatologia_clicked();
+
+    void on_tablaPatologias_doubleClicked();
+
+    void on_btnBuscarMolcula_clicked();
+
+    void on_btnBuscarMedicamentoporMolecula_clicked();
+
+    void on_tablamedicamentosMolecula_clicked();
 
 private:
     Ui::FrmAnadirMedicamento *ui;
