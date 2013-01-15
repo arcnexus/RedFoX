24ee79fe0622daa2cbe9a4eae952ebd3f01d5cb0
arcnexus <marcmirallesbiosca@gmail.com>
2013-01-15 11:54:40 +0100

    test

--- main.cpp
+++ main.cpp
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
         QTextCodec::setCodecForTr(linuxCodec);
         QTextCodec::setCodecForCStrings(linuxCodec);
    #endif
-        QTextCodec::setCodecForLocale(linuxCodec);;
+   QTextCodec::setCodecForLocale(linuxCodec);;
 
    a.setStyle("fusion");
 
