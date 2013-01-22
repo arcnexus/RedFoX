f25be1d3af94dd476fe772dae9718fb6caf627c3
arcnexus <marcmirallesbiosca@gmail.com>
2013-01-22 23:36:30 +0100

    Seleccionar Episodio en QTreeWidget en FrmFichaPaciente Creacion clase visitas.cpp

---
+++ visitas.h
@@ -0,0 +1,18 @@
+#ifndef VISITAS_H
+#define VISITAS_H
+
+#include <QObject>
+
+class Visitas : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Visitas(QObject *parent = 0);
+    
+signals:
+    
+public slots:
+    
+};
+
+#endif // VISITAS_H
