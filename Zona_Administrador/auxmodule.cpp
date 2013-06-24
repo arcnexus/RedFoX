d16765a7cc8a6dcd6b6a78a75b3d575a38212382
0thefox0 <0thefox0@gmail.com>
2013-06-24 17:01:15 +0200

    Mised files

---
+++ Zona_Administrador/auxmodule.cpp
@@ -0,0 +1,59 @@
+#include "auxmodule.h"
+
+QSqlTableModel* AuxModule::_model;
+bool AuxModule::_modelIsSet = false;
+
+AuxModule::AuxModule(QWidget *parent, QString label) :
+    QWidget(parent),
+    _label(label,this),
+    _layout(this),
+    _combo(this)
+{
+    name = label;
+    if(!_modelIsSet)
+        AuxModule::setModel();
+
+    _combo.setModel(AuxModule::model());
+    _combo.setModelColumn(1);
+
+    _layout.addWidget(&_label);
+    _layout.addWidget(&_combo);
+    this->setLayout(&_layout);
+}
+
+AuxModule::~AuxModule()
+{
+    if(AuxModule::_model)
+        AuxModule::_model->deleteLater();
+    _modelIsSet = false;
+}
+
+
+int AuxModule::nivel()
+{
+    return AuxModule::model()->index(_combo.currentIndex(),0).data().toInt();
+}
+
+void AuxModule::setNivel(int idAcceso)
+{
+     for (int i = 0 ; i< AuxModule::model()->rowCount();i++)
+     {
+         if(AuxModule::model()->index(i,0).data().toInt() == idAcceso)
+         {
+             _combo.setCurrentIndex(i);
+             break;
+         }
+     }
+}
+
+QSqlTableModel *AuxModule::model()
+{
+    return _model;
+}
+
+void AuxModule::setModel()
+{
+    _model = new QSqlTableModel(0,QSqlDatabase::database("Maya"));
+    _model->setTable("nivelacceso");
+    _model->select();
+}
---
+++ Zona_Administrador/auxmodule.h
@@ -0,0 +1,35 @@
+#ifndef AUXMODULE_H
+#define AUXMODULE_H
+
+#include <QWidget>
+#include "../mayamodule.h"
+class AuxModule : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit AuxModule(QWidget *parent = 0, QString label="");
+    ~AuxModule();
+    int id;
+    MayaModule::moduleZone zone;
+    QString name;
+
+    int nivel();
+    void setNivel(int idAcceso);
+
+    static QSqlTableModel *model();
+    static void setModel();
+
+signals:
+    
+public slots:
+private:
+    QLabel _label;
+    QComboBox _combo;
+    QHBoxLayout _layout;
+
+    static QSqlTableModel *_model;
+    static bool _modelIsSet;
+
+};
+
+#endif // AUXMODULE_H
